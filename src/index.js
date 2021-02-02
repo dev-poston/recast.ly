94cd576b952260218ad653c4d38fc24de9684dad
Devon Poston <dev.poston@gmail.com>
2021-02-01 19:25:42 -0800

    Complete rendering App component to the DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
+console.log('App:', App);
 
+//ReactDOM.render(<App />, document.getElementByID('app'));
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
