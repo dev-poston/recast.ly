c065f4642ad0d8900b3219bcf3151d60c53dd878
Devon Poston <dev.poston@gmail.com>
2021-02-04 19:06:23 -0800

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
-var searchYouTube = (options, callback) => {
-  // TODO
+import apiKey from '../config/youtube.js';
+
+var searchYouTube = ({query, max = 5}, callback) => {
+  $.ajax({
+    type: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: {
+      key: apiKey,
+      q: query,
+      part: 'snippet',
+      maxResults: max,
+      type: 'video',
+      videoEmbeddable: true,
+    },
+    success: function(data) {
+      callback(data);
+    },
+    error: function(response) {
+      console.log('Request Failed');
+    }
+  });
 };
 
 export default searchYouTube;
